76b31eb80965ab32b515bbe6623f93f8dc659fa8
Facundo <rusmanfacundo@gmail.com>
2022-11-17 15:31:12 -0300

    abm familia

--- routes/familiaProducto.js
+++ routes/familiaProducto.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const conexion = require('../database');
 const router = express.Router();
+const { format } = require('date-fns');
 
 router.post('/crear', async (req, res, next) => {
-  const {descripcion,DescBreve, unidadesFijasPallet, porcRelacionPallet, fechaGraba,usuarioGraba } = req.body;
+  const {descripcion,DescBreve, unidadesFijasPallet, porcRelacionPallet,usuarioGraba } = req.body;
+  const fechaCambiada = format(Date.parse(new Date()), 'yyyy-MM-dd');
   conexion.query(
     'INSERT INTO tipofamiliaproducto (descripcion,DescBreve, unidadesFijasPallet, porcRelacionPallet, fechaGraba,usuarioGraba) VALUES (?,?,?,?,?,?); ',
-    [descripcion,DescBreve, unidadesFijasPallet, porcRelacionPallet, fechaGraba,usuarioGraba],
+    [descripcion,DescBreve, unidadesFijasPallet, porcRelacionPallet, fechaCambiada,usuarioGraba],
     (error, rows) => {
       if (error) {
         console.log(error);
@@ -27,8 +29,8 @@ router.get('', (req, res, next) => {
 });
 
 router.get('/:id', (req, res, next) => {
-  const { idTipoFamiliaProducto  } = req.params;
-  conexion.query('SELECT * FROM tipofamiliaproducto WHERE idTipoFamiliaProducto  = ?', [idTipoFamiliaProducto ], (err, rows, fields) => {
+  const { id  } = req.params;
+  conexion.query('SELECT * FROM tipofamiliaproducto WHERE idTipoFamiliaProducto  = ?', [id ], (err, rows, fields) => {
     if (!err) {
       res.json(rows);
     } else {
@@ -38,11 +40,12 @@ router.get('/:id', (req, res, next) => {
 });
 
 router.put('/:id', (req, res) => {
-  const { idTipoFamiliaProducto  } = req.params;
-  const { descripcion,DescBreve, unidadesFijasPallet, porcRelacionPallet, fechaModifica,usuarioModifica } = req.body;
+  const { id  } = req.params;
+  const { descripcion,DescBreve, unidadesFijasPallet, porcRelacionPallet,usuarioModifica, usuarioGraba } = req.body;
+  const fechaCambiada = format(Date.parse(new Date()), 'yyyy-MM-dd');
   conexion.query(
-    'UPDATE tipofamiliaproducto SET descripcion = ?, DescBreve = ?, unidadesFijasPallet = ?,porcRelacionPallet = ?, fechaModifica = ?, usuarioModifica = ? WHERE idTipoFamiliaProducto  = ?',
-    [ descripcion,DescBreve, unidadesFijasPallet, porcRelacionPallet, fechaModifica,usuarioModifica, idTipoFamiliaProducto ],
+    'UPDATE tipofamiliaproducto SET descripcion = ?, DescBreve = ?, unidadesFijasPallet = ?,porcRelacionPallet = ?, usuarioGraba = ?, fechaModifica = ?, usuarioModifica = ? WHERE idTipoFamiliaProducto  = ?',
+    [ descripcion,DescBreve, unidadesFijasPallet, porcRelacionPallet,usuarioGraba, fechaCambiada,usuarioModifica, id ],
     (err, rows, fields) => {
       if (!err) {
         res.json({ Status: 'Tipo Familia Producto Actualizado' });
@@ -54,8 +57,8 @@ router.put('/:id', (req, res) => {
 });
 
 router.delete('/:id', (req, res) => {
-  const { idTipoFamiliaProducto  } = req.params;
-  conexion.query('DELETE FROM tipofamiliaproducto WHERE idTipoFamiliaProducto  = ?', [idTipoFamiliaProducto], (err, rows, fields) => {
+  const { id  } = req.params;
+  conexion.query('DELETE FROM tipofamiliaproducto WHERE idTipoFamiliaProducto  = ?', [id], (err, rows, fields) => {
     if (!err) {
       res.json({ Status: 'Tipo Familia Producto eliminado' });
     } else {
