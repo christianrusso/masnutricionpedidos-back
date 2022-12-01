5972dfb839c6390dd4879e8787d468c998b7bb9d
Facundo <rusmanfacundo@gmail.com>
2022-12-01 09:46:54 -0300

    conexion db

--- database.js
+++ database.js
@@ -1,16 +1,16 @@
 const mysql = require('mysql');
 
-const conexion = mysql.createConnection({
-  host: 'localhost',
-  user: 'masnutricionpedidos',
-  password: 'masnutricionpedidos', //'1682951!Abmtoba',
-  database: 'masnutricionpedidos'
+const conexion = mysql.createPool({
+  host: 'us-cdbr-east-05.cleardb.net',
+  user: 'b45d2c3c7dab74',
+  password: 'fb1a5735',
+  database: 'heroku_2c952474016e43f'
 });
-conexion.connect(error => {
-  if (error) {
-    throw error;
-  }
-  console.log('¡Conectado a la base de datos MySQL!');
-});
+// conexion.connect(error => {
+//   if (error) {
+//     throw error;
+//   }
+//   console.log('¡Conectado a la base de datos MySQL!');
+// });
 
 module.exports = conexion;
