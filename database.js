2342b273ddbfc8c53a145f90a49b697559e5c025
lucasrusman <rusmanlucas@gmail.com>
2022-12-04 23:23:06 -0300

    Correccion del acceso a la base de datos

--- database.js
+++ database.js
@@ -1,28 +1,17 @@
 const mysql = require('mysql');
 
-// const conexion = mysql.createPool({
-   // host: 'us-cdbr-east-06.cleardb.net',
-   // user: 'b45d2c3c7dab74',
-   // password: 'fb1a5735',
-   // database: 'heroku_2c952474016e43f'
+ const conexion = mysql.createPool({
+   host: 'us-cdbr-east-06.cleardb.net',
+   user: 'b45d2c3c7dab74',
+   password: 'fb1a5735',
+   database: 'heroku_2c952474016e43f'
   
-// });
+});
 // conexion.connect(error => {
 //   if (error) {
 //     throw error;
 //   }
 //   console.log('¡Conectado a la base de datos MySQL!');
 // });
-const conexion = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'masnutricionpedidos'
-});
-conexion.connect(function(err) {
-  if (err) {
-    throw err
-  }
-  console.log('¡Conectado a la base de datos MySQL!');
-});
+
 module.exports = conexion;
